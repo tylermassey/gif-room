734a1f09a172a7f7d255cc01e0e4d7aa6ac12b7f
Tyler Massey <tylermassey6@gmail.com>
2016-10-09 16:03:15 -0500

    Adding credentials.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDdfMOAYzhzTBm1UQm_n-2jrro6raPWHc",
+    authDomain: "gif-room.firebaseapp.com",
+    databaseURL: "https://gif-room.firebaseio.com",
+    storageBucket: "gif-room.appspot.com",
+    messagingSenderId: "274036202170"
+  }
 };
